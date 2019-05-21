58e0573252f115379f68d7903c36bb12bcb195c3
Anthony Ontiveros <anthony.ontiveros.d@gmail.com>
2019-05-21 11:38:01 -0700

    testing environment setup

---
+++ client/src/js/main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './components/App';
+
+reactDOM.render(
+    <Router>
+        <App />
+    </Router>
+    , document.getElementById('root')
+);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,27 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-
+    entry: './src/js/main.js',
+    output: {
+        filename: 'bundle.js',
+        publicPath: './static/js/',
+        path: path.resolve(__dirname, '..', 'server', 'webapp', 'dist', 'static', 'js')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                include: path.resolve(__dirname, 'src', 'js'),
+                exclude: path.resolve(__dirname, 'node_modules')
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: '../../index.html'
+        })
+    ]
 };
